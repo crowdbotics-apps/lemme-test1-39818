398e5fb5fdef1a8ea90b5bedf5e19c18a150319a
Crowdbotics <team@crowdbotics.com>
2023-04-10 19:05:33 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lemme-test1-39818",
+  url: "https://lemme-test1-39818.botics.co",
+  api: "https://lemme-test1-39818.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
